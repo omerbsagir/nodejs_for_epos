ba028c8ed115f30c13712e917e0f6976facf9b2b
omerbsagir <omerbesagir@gmail.com>
2024-07-30 15:18:15 +0300

    Authentication + JWT

--- src/handlers/authHandlers.js
+++ src/handlers/authHandlers.js
@@ -4,56 +4,58 @@ const bcrypt = require('bcryptjs');
 const { v4: uuidv4 } = require('uuid');
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
 const cognito = new AWS.CognitoIdentityServiceProvider();
+
+const JWT_SECRET = process.env.JWT_SECRET; // JWT imzalama anahtarı
+const JWT_EXPIRES_IN = '1h'; // JWT'nin geçerlilik süresi
 
 const handleLogin = async (event) => {
     const { email, password } = JSON.parse(event.body);
-    
+
     const params = {
-        AuthFlow: 'USER_PASSWORD_AUTH',
-        ClientId: process.env.COGNITO_CLIENT_ID,
-        AuthParameters: {
-            USERNAME: email,
-            PASSWORD: password
-        }
+        TableName: process.env.USERS_TABLE,
+        Key: { email }
     };
 
     try {
+        const result = await dynamoDb.get(params).promise();
+        const user = result.Item;
 
-        const authResult = await cognito.initiateAuth(params).promise();
+        if (!user || !await bcrypt.compare(password, user.password)) {
+            return {
+                statusCode: 401,
+                body: JSON.stringify({ message: 'Invalid email or password' })
+            };
+        }
 
-        if (!authResult.AuthenticationResult || !authResult.AuthenticationResult.IdToken) {
-            console.error("AuthenticationResult: ", authResult);
-            throw new Error("Authentication failed. IdToken is missing.");
-          }
-        
-        const idToken = authResult.AuthenticationResult.IdToken;
-        const accessToken = authResult.AuthenticationResult.AccessToken;
-        const refreshToken = authResult.AuthenticationResult.RefreshToken;
+        // JWT oluşturma
+        const token = jwt.sign(
+            { email: user.email, userId: user.userId },
+            JWT_SECRET,
+            { expiresIn: JWT_EXPIRES_IN }
+        );
 
         return {
             statusCode: 200,
             body: JSON.stringify({
                 message: 'Login successful',
-                idToken,
-                accessToken,
-                refreshToken
+                token
             })
         };
     } catch (error) {
         console.error("Login error: ", error);
 
         return {
-            statusCode: 401,
+            statusCode: 500,
             body: JSON.stringify({ message: 'Login failed', error: error.message })
         };
     }
 };
 
+
 const handleRegister = async (event) => {
     const { email, phone, password } = JSON.parse(event.body);
-    
+
     const userId = uuidv4(); // Benzersiz kullanıcı ID'si oluşturma
     const hashedPassword = await bcrypt.hash(password, 10); // Parolayı hashleme
     const createdAt = new Date().toISOString();
@@ -100,5 +102,6 @@ const handleRegister = async (event) => {
         };
     }
 };
+
 
 module.exports = { handleLogin ,handleRegister };
