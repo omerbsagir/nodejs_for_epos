8bfcc65c924b7a70b01a31bd20fb112c5fcd3f58
omerbsagir <omerbesagir@gmail.com>
2024-07-31 10:34:04 +0300

    dbHandlers index.js ye eklendi

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const userHandlers = require('./src/handlers/userHandlers');
 const activationHandlers = require('./src/handlers/activationHandlers');
 const walletHandlers = require('./src/handlers/walletHandlers');
 const nfcHandlers = require('./src/handlers/nfcHandlers');
+const dbHandlers = require('./src/handlers/dbHandlers');
 
 exports.handler = async (event) => {
     try {
